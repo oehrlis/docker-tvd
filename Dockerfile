430575c3004f852926eecfb2e8026cb503d69469
Stefan Oehrli <stefan.oehrli@trivadis.com>
2017-09-22 16:48:32 +0200

    Change Dockerfile to setup script rather than RUN commands

--- Dockerfile
+++ Dockerfile
@@ -25,53 +25,9 @@ FROM oraclelinux:7-slim
 # Maintainer
 # ----------------------------------------------------------------------
 MAINTAINER Stefan Oehrli <stefan.oehrli@trivadis.com>
+# copy all scripts
 
-#Common ENV
-# ----------------------------------------------------------------------
-ENV ORACLE_ROOT=/u00 \
-    ORACLE_DATA=/u01 \
-    ORACLE_BASE=/u00/app/oracle
+ADD scripts /opt/docker/bin/
 
-# Setup user, directory and update yum packages
-# ----------------------------------------------------------------------
-RUN mkdir -p $ORACLE_ROOT $ORACLE_DATA $ORACLE_BASE && \ 
-    chmod a+xr $ORACLE_ROOT $ORACLE_DATA && \
-
-# create oracle groups
-    groupadd --gid 1000 oinstall && \
-    groupadd --gid 1010 osdba && \
-    groupadd --gid 1020 osoper && \
-    groupadd --gid 1030 osbackupdba && \
-    groupadd --gid 1040 oskmdba && \
-    groupadd --gid 1050 osdgdba && \
-
-# create oracle user
-    useradd --create-home --gid oinstall --shell /bin/bash \
-        --groups oinstall,osdba,osoper,osbackupdba,osdgdba,oskmdba \
-        oracle && \
-
-# create oracle directories
-    mkdir -p $ORACLE_BASE/etc $ORACLE_BASE/local && \
-    chown oracle:oinstall -R $ORACLE_ROOT $ORACLE_DATA && \
-
-# create an oraInst.loc file
-    echo "inventory_loc=$ORACLE_BASE/oraInventory" > $ORACLE_BASE/etc/oraInst.loc && \
-    echo "inst_group=oinstall" >> $ORACLE_BASE/etc/oraInst.loc && \
-
-# update existing packages
-    yum upgrade -y && \
-# install util-linux, libaio, perl and Oracle 12.2 PreInstall 
-    yum install -y oracle-database-server-12cR2-preinstall \
-        libaio perl perl-core perl-IO-Socket-SSL util-linux && \
-    yum clean all && \
-    rm -rf /var/cache/yum
-
-# Prepare oracle environment 
-# ----------------------------------------------------------------------
-USER oracle
-
-VOLUME ["$ORACLE_DATA"]
-
-# Define default command to start script.
-# overwrite this with 'CMD []' in a dependent Dockerfile
-CMD ["/bin/bash"]
+# image setup via shell script to reduce layers and optimize final disk usage
+RUN /opt/docker/bin/setup_tvd.sh
---
+++ scripts/setup_tvd.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# ----------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ----------------------------------------------------------------------
+# Name.......: setup_tvd.sh 
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2017.09.22
+# Revision...: 
+# Purpose....: Setup script for docker base image 
+# Notes......: --
+# Reference..: --
+# License....: CDDL 1.0 + GPL 2.0
+# ----------------------------------------------------------------------
+# Modified...: 
+# see git revision history for more information on changes/updates
+# TODO.......: 
+# ----------------------------------------------------------------------
+
+# ignore secure linux (not available on slim linux
+#setenforce Permissive
+
+# define environment variables
+export ORACLE_ROOT=/u00             # oracle root directory
+export ORACLE_DATA=/u01             # oracle data directory
+export ORACLE_BASE=/u00/app/oracle  # oracle base directory
+
+# create oracle groups
+groupadd --gid 1000 oinstall
+groupadd --gid 1010 osdba
+groupadd --gid 1020 osoper
+groupadd --gid 1030 osbackupdba
+groupadd --gid 1040 oskmdba
+groupadd --gid 1050 osdgdba && \
+
+# create oracle user
+useradd --create-home --gid oinstall --shell /bin/bash \
+    --groups oinstall,osdba,osoper,osbackupdba,osdgdba,oskmdba \
+    oracle
+
+# create oracle directories
+mkdir -p $ORACLE_ROOT
+mkdir -p $ORACLE_DATA
+mkdir -p $ORACLE_BASE
+mkdir -p $ORACLE_BASE/admin         # softlink to volume
+mkdir -p $ORACLE_BASE/audit         # softlink to volume
+mkdir -p $ORACLE_BASE/cfgtoollogs
+mkdir -p $ORACLE_BASE/diag          # softlink to volume
+mkdir -p $ORACLE_BASE/etc           # softlink to volume
+mkdir -p $ORACLE_BASE/local
+mkdir -p $ORACLE_BASE/network       # softlink to volume
+mkdir -p $ORACLE_BASE/product
+
+# change permissions and ownership
+chmod a+xr $ORACLE_ROOT $ORACLE_DATA
+chown oracle:oinstall -R $ORACLE_ROOT $ORACLE_DATA
+
+# create an oraInst.loc file
+echo "inventory_loc=$ORACLE_BASE/oraInventory" > $ORACLE_BASE/etc/oraInst.loc
+echo "inst_group=oinstall" >> $ORACLE_BASE/etc/oraInst.loc
+
+# update existing packages
+yum upgrade -y
+
+# install basic packages util-linux, libaio 
+yum install -y libaio util-linux
+
+# intall oracle preinstall package
+#yum install -y oracle-database-server-12cR2-preinstall
+
+# install perl packages
+#yum install -y perl perl-core perl-IO-Socket-SSL \
+
+# clean up
+yum clean all
+rm -rf /var/cache/yum
